1f4a60f05d44595fdf3870bd4677588c855ab749
Victor-maestro <victor_maestro@mail.ru>
2023-10-29 18:50:00 +0600

    Add Module and Data, Util modules

---
+++ js/util.js
@@ -0,0 +1,24 @@
+function getRandomNumber(min, max, digits = 1) {
+  const lower = Math.min(Math.abs(min), Math.abs(max));
+  const upper = Math.max(Math.abs(min), Math.abs(max));
+
+  const randomNumber = Math.random() * (upper - lower + 1) + lower;
+
+  return +randomNumber.toFixed(digits);
+}
+
+
+function getRandomElements(arr) {
+
+  const randomElements = [];
+  const cloneArray = arr.slice();
+
+  for (let i = 0; i < getRandomNumber(0, arr.length, 0); i++) {
+    const randomIndex = Math.floor(Math.random() * cloneArray.length);
+    randomElements.push(cloneArray.splice(randomIndex, 1)[0]);
+  }
+
+  return randomElements;
+}
+
+export {getRandomNumber, getRandomElements};
